a6c567180611ab18dde1926989630b85298be0d2
quantumquadtrees8523 <surya.duggirala96@gmail.com>
2024-09-29 13:52:27 -0400

    test 123

--- app/src/components/BlogPage.js
+++ app/src/components/BlogPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
-import Post from './Post';
+import Post from './PostMain';
 import {api} from '../services/Firebase';
 
 function BlogPage({ 
